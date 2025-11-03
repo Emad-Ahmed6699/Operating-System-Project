13dab3e1bc5143111ec3be1ffb0c73dd699ad6e6
Emad-Ahmed6699 <Emad.Elden2974@compit.aun.edu.eg>
2025-11-03 22:40:33 +0200

    implement class Converter that will exposes two functions StringToBytes and BytesToString.

---
+++ Converter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fat_file_system_cs
+{
+    internal class Converter
+    {
+        public static byte[] StringToBytes(string text)
+        {
+            return System.Text.Encoding.UTF8.GetBytes(text);
+        }
+
+        public static string BytesToString(byte[] bytes)
+        {
+            return System.Text.Encoding.UTF8.GetString(bytes).TrimEnd('\0');
+        }
+    }
+}
